61fe8990cc5e82e38b9fc0c775b0e7629f71965f
Devlin Harris <devlin2313@gmail.com>
2022-07-02 12:12:39 -0500

    adding project test

---
+++ src/second/Application.java
@@ -0,0 +1,37 @@
+package second;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// this is a comment
+		/*
+		 * primitive:
+		 * int - an integer
+		 * double - decimal places 32.89325
+		 * float - precision decimal data type 45.3987
+		 * long - like an int, numbers can be higher
+		 * short - like an int, less of a number range
+		 * byte - 8 bits of data
+		 * char - a single character
+		 * boolean - true or false
+		 * 
+		 * objects
+		 * String - is textual data, a string of characters
+		 */
+		
+		//variable declaration - type, identifier, assignment operator, value, semicolon
+		int numberOfFriends;
+		int samsAge = 6;
+		System.out.println(samsAge);
+		double accountBalance = 34.67;
+		System.out.println(accountBalance);
+		char middleInitial = 'c';
+		boolean isHotOutside = true;
+		String firstName = "Sam";
+				System.out.println(middleInitial);
+		System.out.println(isHotOutside);
+		System.out.println(firstName);
+		
+	}
+
+}
